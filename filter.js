fdbdf73f1e12395ea9f4c1cc38b6a745c98df0d0
Soclemys <soclemys@gmail.com>
2022-03-13 23:29:53 +0200

    fuck facebook

--- filter.js
+++ filter.js
@@ -1,3 +1,20 @@
+const FILTERS = {
+    full: element => {
+        element.remove()
+    },
+    hard: element => {
+        element.innerHTML = settings.censorshipString;
+    },
+    fix: element => {
+        settings.fixers.forEach(expression => {
+            element.innerText = element.innerText.replaceAll(expression.expression, expression.fix);
+        })
+    },
+    soft: element => {
+        element.style.textDecoration = 'line-through';
+    }
+}
+
 const REGEX = {
     interval: {
         match: ' (?=[\.,!?])',
@@ -17,17 +34,38 @@ const getSettings = async () => {
     });
 }
 
+const getFilter = () => {
+    let regexp = '';
+    settings.filterMatch.forEach(pattern => {
+        regexp += '|' + REGEX[pattern].match;
+    })
+    if (settings.filterType == 'fix') {
+        settings.fixers = []
+        settings.filterMatch.forEach(pattern => {
+            settings.fixers.push({
+                expression: new RegExp(REGEX[pattern].match, 'g'),
+                fix: REGEX[pattern].fix
+            });
+        })
+    }
+    regexp = regexp.substring(1);
+    let filter = FILTERS[settings.filterType];
+    let capture = element => {
+        if (element.innerText.match(regexp)) {
+            console.log('Filtered element: ', element);
+            filter(element);
+        }
+    }
+    return capture;
+}
+
 const applyFilter = filter => {
     switch (window.location.hostname) {
 
         case 'novini.bg':
             window.addEventListener('load', () => {
-                let content = []
-                let articleParagraphs = document.getElementsByClassName('openArticle__content')[0].getElementsByTagName('p');
                 let comments = document.getElementsByClassName('comment__content');
-                Array.prototype.push.apply(content, articleParagraphs);
-                Array.prototype.push.apply(content, comments);
-                Array.prototype.forEach.call(content, element => {
+                Array.prototype.forEach.call(comments, element => {
                     filter(element);
                 })
             });
@@ -38,55 +76,153 @@ const applyFilter = filter => {
                 childList: true,
                 subtree: true
             }
-            let commentObserver = new MutationObserver((mutations, obs) => {
-                let commentsContainer = document.getElementsByTagName('ytd-comments')[0];
-                if (commentsContainer) {
-                    let contentDiv = commentsContainer.querySelector('#contents');
-                    if (contentDiv) {
-                        let newCommentObserver = new MutationObserver((mutations) => {
-                            let mutation = mutations[0];
-                            if (
-                                mutation.type === 'childList' &&
-                                mutation.addedNodes.length &&
-                                mutation.addedNodes[0].tagName === 'YTD-COMMENT-THREAD-RENDERER'
-                            ) {
-                                filter(mutation.addedNodes[0].querySelector('yt-formatted-string#content-text'));
-                            }
-                        });
-                        newCommentObserver.observe(contentDiv, observerSettings);
+
+            // the following is for comments
+            let commentSectionObserver = new MutationObserver((mutations, obs) => {
+                mutations.forEach(mutation => {
+                    if (
+                        mutation.type === 'childList' &&
+                        mutation.addedNodes.length &&
+                        mutation.target.id === 'comments'
+                    ) {
+                        let contentsDiv = mutation.addedNodes[3].childNodes[5];
+                        let commentObserver = new MutationObserver(mutations => {
+                            mutations.forEach(mutation => {
+                                if (
+                                    mutation.type === 'childList' &&
+                                    mutation.addedNodes.length &&
+                                    mutation.target.id === 'content-text'
+                                ) {
+                                    filter(mutation.target);
+                                }
+                            })
+                        })
+                        commentObserver.observe(contentsDiv, observerSettings);
                         obs.disconnect();
                     }
-                }
+                })
             });
-            commentObserver.observe(document, observerSettings);
+            commentSectionObserver.observe(document, observerSettings);
+
+            // the following is for live chat
             let iFrameObserver = new MutationObserver((mutations, obs) => {
-                let chatFrame = document.getElementById('chatframe');
-                if (chatFrame) {
-                    let chatWindow = chatFrame.contentWindow;
-                    chatWindow.addEventListener('load', e => {
-                        let chatItems = chatWindow.document.getElementById('item-offset').childNodes[1];
-                        chatItems.childNodes.forEach(element => {
-                            if (element.tagName === 'YT-LIVE-CHAT-TEXT-MESSAGE-RENDERER') {
-                                filter(element.querySelector('#message'));
-                            }
+                mutations.forEach(mutation => {
+                    if (
+                        mutation.type === 'childList' &&
+                        mutation.addedNodes.length &&
+                        mutation.target.id === 'secondary-inner' &&
+                        (mutation.addedNodes[1] && mutation.addedNodes[1].tagName === 'YTD-LIVE-CHAT-FRAME')
+                    ) {
+                        let chatWindow = mutation.addedNodes[1].querySelector('iframe').contentWindow;
+                        chatWindow.addEventListener('load', e => {
+                            let chatItems = chatWindow.document.getElementById('item-offset').childNodes[1];
+                            chatItems.childNodes.forEach(element => {
+                                if (element.tagName === 'YT-LIVE-CHAT-TEXT-MESSAGE-RENDERER') {
+                                    filter(element.querySelector('#message'));
+                                }
+                            })
+                            let chatObserver = new MutationObserver(mutations => {
+                                mutations.forEach(mutation => {
+                                    if (mutation.target.id == 'message') { 
+                                        filter(mutation.target);
+                                    }
+                                })
+                            });
+                            chatObserver.observe(chatItems, observerSettings);
                         })
-                        let chatObserver = new MutationObserver(mutations => {
-                            let mutation = mutations[0];
-                            if (
-                                mutation.type === 'childList' &&
-                                mutation.addedNodes.length &&
-                                mutation.addedNodes[0].tagName === 'YT-FORMATTED-STRING'
-                            ) {
-                                filter(mutation.addedNodes[0].querySelector('#message'));
-                            }
-                        });
-                        chatObserver.observe(chatItems, observerSettings);
-                    })
-                    obs.disconnect();
-                    return;
-                }
+                        obs.disconnect();
+                    }
+                });
             });
             iFrameObserver.observe(document, observerSettings);
+
+            break;
+
+        case 'www.facebook.com':
+            // fuck facebook for this among other things
+            let retrieveCommentElement = el => {
+                // console.log(el);
+                if (el.childNodes.length > 1) {
+                    el = el.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0];
+                    if (el.childNodes.length === 3) {
+                        el = el.childNodes[2];
+                    } else {
+                        el = el.childNodes[1];
+                    }
+                } else {
+                    if (el.childNodes[0].childNodes[1]) {
+                        el = el.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[0];
+                        if (el.childNodes.length > 1) {
+                            el = el.childNodes[1];
+                        } else {
+                            el = el.childNodes[0].childNodes[0];
+                            if (el.childNodes.length == 2) {
+                                el = el.childNodes[1];
+                            } else {
+                                el = el.childNodes[2];
+                            }
+                        }
+                    } else {
+                        console.log('THIS IS THE ERROR');
+                        console.log(el.childNodes[0].childNodes[1]);
+                    }
+                }
+                return el
+            }
+
+            observer = new MutationObserver((mutations, observer) => {
+                mutations.forEach(mutation => {
+                    // this captures top-level comments that load in
+                    if (
+                        mutation.type === 'childList' &&
+                        mutation.addedNodes.length &&
+                        (mutation.addedNodes[0].tagName === 'LI' && mutation.target.tagName === 'UL')
+                    ) {
+                        let el = retrieveCommentElement(mutation.addedNodes[0].childNodes[0]);
+                        if (el) filter(el);
+                        
+                    // this captures posts that load in
+                    } else if (
+                        mutation.type === 'childList' &&
+                        mutation.addedNodes.length &&
+                        (mutation.addedNodes[0].dataset && mutation.addedNodes[0].dataset.pagelet === 'FeedUnit_{n}')
+                    ) {
+                        let el = mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[2];
+                        if (el && el.childNodes[0].dir == 'auto') filter(el.childNodes[0]);
+                    // this captures comment replies
+                    } else if (
+                        mutation.type === 'childList' &&
+                        mutation.addedNodes.length &&
+                        (mutation.addedNodes[0].childNodes[1] && mutation.addedNodes[0].childNodes[1].tagName === 'UL')
+                    ) {
+                        mutation.addedNodes[0].childNodes[1].childNodes.forEach(node => {
+                            node.childNodes[0].childNodes.forEach(childNode => {
+                                if (childNode.ariaLabel) {
+                                    let el = retrieveCommentElement(childNode);
+                                    if (el) filter(el);
+                                }
+                            });
+                        });
+                    } else if (
+                        mutation.type === 'childList' &&
+                        mutation.addedNodes.length &&
+                        (mutation.addedNodes[0].childNodes[0] && mutation.addedNodes[0].childNodes[0].tagName === 'UL')
+                    ) {
+                        mutation.addedNodes[0].childNodes[0].childNodes.forEach(node => {
+                            node.childNodes[0].childNodes.forEach(childNode => {
+                                if (childNode.ariaLabel) {
+                                    let el = retrieveCommentElement(childNode);
+                                    if (el) filter(el);
+                                }
+                            })
+                        });
+                    }
+                })
+            })
+            observer.observe(document, {
+                childList: true,
+                subtree: true
+            })
             break;
 
         default:
@@ -100,52 +236,9 @@ const applyFilter = filter => {
     }
 }
 
-const getFilter = (filterType, filterMatch, censorshipString) => {
-    let regex = '';
-    let expressions = [];
-    filterMatch.forEach(stupid => {
-        regex += '|' + REGEX[stupid].match;
-        expressions.push({
-            expression: new RegExp(REGEX[stupid].match, 'g'),
-            fix: REGEX[stupid].fix
-        });
-    })
-    regex = regex.substring(1);
-    let filter = (() => {
-        switch (filterType) {
-            case 'full':
-                return element => {
-                    element.remove();
-                }
-            case 'hard':
-                return element => {
-                    element.innerHTML = censorshipString;
-                }
-            case 'fix':
-                return element => {
-                    expressions.forEach(expression => {
-                        element.innerText = element.innerText.replaceAll(expression.expression, expression.fix);
-                    })
-                }
-            case 'soft':
-            default:
-                return element => {
-                    element.style.textDecoration = 'line-through';
-                }
-        }
-    })();
-    let capture = element => {
-        if (element.innerText.match(regex)) {
-            console.log('Filtered element: ', element);
-            filter(element);
-        }
-    }
-    return capture;
-}
-
 const init = async () => {
-    let settings = await getSettings();
-    let filter = getFilter(settings.filterType, settings.filterMatch, settings.censorshipString);
+    settings = await getSettings();
+    let filter = getFilter();
     applyFilter(filter);
 };
 
